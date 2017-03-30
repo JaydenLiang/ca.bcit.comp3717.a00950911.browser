b5137f8305fb56b7c6b43bb621eca515da025fbb
Jayden Liang <jaydenliang81@gmail.com>
2017-03-29 22:43:07 -0700

    Initial commit

---
+++ app/src/main/java/ca/bcit/comp3717/a00950911/browser/WebBrowserActivity.java
@@ -0,0 +1,17 @@
+package ca.bcit.comp3717.a00950911.browser;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class WebBrowserActivity extends AppCompatActivity {
+    private String rawURL;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_browser);
+        this.rawURL = getIntent().getStringExtra("rawURL");
+        WebView webView = (WebView) findViewById(R.id.webbrowser_webview);
+        webView.loadUrl(this.rawURL);
+    }
+}
